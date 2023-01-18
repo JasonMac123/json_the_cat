1c1cbb412e60f8b077290402190f433e53389e53
JasonSnow123 <m.jason.2018@gmail.com>
2023-01-18 16:19:24 -0500

    created mocha testing for cat api

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,17 @@
 const request = require('request');
 const apiEndPoint = 'https://api.thecatapi.com/v1/breeds/search?q=';
 
-
-const args = process.argv[2];
-const findCatDescription = function(name) {
-  request(`${apiEndPoint}${name}`, (error, response, body) => {
+const fetchBreedDescription = function(breedName, callback) {
+  request(`${apiEndPoint}${breedName}`, (error, response, body) => {
     if (error) {
-      console.log("Request Failed");
-      return;
+      callback(error, null);
     }
     const data = JSON.parse(body);
     if (data.length === 0) {
-      console.log('Breed cannot be found');
-      return;
+      return data;
     }
-    console.log(data[0].description);
-    return;
+    return callback(null, data[0].description);
   });
 };
-findCatDescription(args);
+
+module.exports = { fetchBreedDescription };
